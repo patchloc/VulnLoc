3c73dbcce5fbff0cdd522aee12c68da35da00f27
shensq04 <shensq04@gmail.com>
2020-07-19 22:19:06 +0800

    update

--- test/test.sh
+++ test/test.sh
@@ -19,7 +19,7 @@ fi
 cp ./config.ini $code_folder
 
 echo "The default number of processes is 10. PatchLoc will adjust it according to the number of cpus on the local machines."
-echo "The default timeout is 2h. The user can change the timeout in ./code/fuzz.py"
+echo "The default timeout is 4h. The user can change the timeout in ./code/fuzz.py"
 echo "The execution progress can be found at the fuzz.log in the output folder. It will not be printed out in the terminal."
 echo "Please do not terminate the execution until PatchLoc timeouts automatically."
 
